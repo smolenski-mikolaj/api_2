97502ead74597765f0dd483e6b6fe83923f0fea5
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-09-25 16:58:27 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '2116',
+	'X-Auth-Token': 'dba600800515acc1fb0f69e2dfb49d26'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+	url: baseUrl + '/board',
+	method: 'GET',
+	success: function(response) {
+	  setupColumns(response.columns);
+	}
+});
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
+}
